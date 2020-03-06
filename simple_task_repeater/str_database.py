677c6d4e0544ba6ab23dc60c05695c51462ac294
Calm Quant <calmquant@gmail.com>
2020-03-06 17:21:02 +0300

    Add todos about making user a class

--- simple_task_repeater/str_database.py
+++ simple_task_repeater/str_database.py
@@ -1,10 +1,13 @@
 from functools import wraps
+from typing import List
 
 from .base import Task
 from .database import Database, synced
 
 
 class STRDatabase(Database):
+    # todo: make user an object, with attributes
+    # todo: rename all str users to username
     @wraps(Database.__init__)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,18 +16,22 @@ class STRDatabase(Database):
         if 'removed_users' not in self.data:
             self.data['removed_users'] = {}  # users_tasks[user][task_key]
 
+    # todo: rename to user_name
     @property
-    def users(self):
+    def user_names(self):
         return list(self._users_tasks.keys())
 
-    def get_users_tasks(self, user):
-        if not self.has_user(user):
-            raise ValueError(f"No user {user}")
-        return [Task.from_json(task) for task in self._users_tasks[user].values()]
+    # todo: rename to user_name
+    def get_users_tasks(self, user_name) -> List[Task]:
+        if not self.has_user(user_name):
+            raise ValueError(f"No user {user_name}")
+        return [Task.from_json(task) for task in self._users_tasks[user_name].values()]
 
+    # todo: rename to user_name
     def has_user(self, user):
         return user in self._users_tasks
 
+    # todo: rename to user_name, add kwargs
     @synced
     def add_user(self, user):
         if self.has_user(user):
@@ -35,6 +42,9 @@ class STRDatabase(Database):
         else:
             self._users_tasks[user] = {}
 
+    # todo: add update_user
+
+    # todo: rename to user_name
     @synced
     def remove_user(self, user):
         if not self.has_user(user):
@@ -46,13 +56,16 @@ class STRDatabase(Database):
     def _users_tasks(self):
         return self.data['users_tasks']
 
+    # todo: rename to user_name
     @property
     def _removed_users(self):
         return self.data['removed_users']
 
+    # todo: rename to user_name
     def has_task(self, user, shortcut):
         return self.has_user(user) and shortcut in self._users_tasks[user]
 
+    # todo: rename to user_name
     @synced
     def add_task(self, task: Task):
         if not self.has_user(task.user):
@@ -61,17 +74,20 @@ class STRDatabase(Database):
             raise ValueError(f"Already have task {task.shortcut} for user {task.user}")
         self._users_tasks[task.user][task.shortcut] = task.to_json()
 
+    # todo: rename to user_name
     def get_task(self, user, shortcut):
         if not self.has_task(user, shortcut):
             raise ValueError(f"No task {shortcut} for user {user}")
         return Task.from_json(self._users_tasks[user][shortcut])
 
+    # todo: rename to user_name
     @synced
     def update_task(self, task: Task):
         if not self.has_task(task.user, task.shortcut):
             raise ValueError(f"No task {task.shortcut} for user {task.user}")
         self._users_tasks[task.user][task.shortcut] = task.to_json()
 
+    # todo: rename to user_name
     @synced
     def remove_task(self, user, shortcut):
         if not self.has_task(user, shortcut):
